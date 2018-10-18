f51f73ec9155724f76c92f238e762118dee48374
BuildTools <unconfigured@null.spigotmc.org>
2018-10-17 23:02:30 -0400

    done btw

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner stringReader = new Scanner (System.in);
+        String userInput;
+        String[] sp;
+
+        while (true){
+            System.out.println("Type in a sentence and press ENTER.");
+            userInput = stringReader.nextLine();
+            userInput = userInput.toUpperCase();
+            userInput += ":)";
+            sp = userInput.split("S\\s*A");
+
+            if (userInput.equals("EXIT:)")) {
+                break;
+
+
+            }
+            System.out.println("There are " + (sp.length - 1) + " occurrences. \n");
+        }
     }
 }
